d05e7542f6419c51145b732ede9c9b0500035d02
Jordan Wang <jordanwang@Jordans-MacBook.local>
2020-09-01 14:38:01 -0700

    update dburl

--- app.js
+++ app.js
@@ -1,57 +1,64 @@
-var express         = require("express"),
-    app             = express(),
-    bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose"),
-    flash           = require("connect-flash"),
-    passport        = require("passport"),
-    LocalStrategy   = require("passport-local"),
-    methodOverride  = require("method-override"),
-    Campground      = require("./models/campground"),
-    Comment         = require("./models/comment"),
-    User            = require("./models/user"),
-    seedDB          = require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
-//requiring routes 
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index")
+//requiring routes
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://dbWang:test12345@cluster0.l51tg.mongodb.net/<dbname>?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log("Connected to DB!");
-}).catch(err => {
-    console.log("ERROR:", err.message);
+// mongoose.connect("mongodb://localhost/yelp_camp", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.DATABASEURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+// mongoose.connect("mongodb+srv://dbWang:test12345@cluster0.l51tg.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// }).then(()=>{
+//     console.log("Connected to DB!");
+// }).catch(err => {
+//     console.log("ERROR:", err.message);
+// });
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+ "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-// seedDB();  //adding intial data to be viewed 
+// seedDB();  //adding intial data to be viewed
 
 // Passport Cofiguration
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "This can be whatever",
     resave: false,
-    saveUninitialized: false
-})); 
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser()); //from passport local mongosse
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
@@ -59,6 +66,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 var port = process.env.PORT || 5000;
-app.listen(port, function(){
-    console.log("Yelp Server 5000");
+app.listen(port, function () {
+  console.log("Yelp Server 5000");
 });
